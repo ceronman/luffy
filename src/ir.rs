4a187b263e7732b81abfda6d5b5e11ed8591c324
Manuel Ceron <ceronman@gmail.com>
2026-05-15 20:59:18 +0200

    Added an initial version of a Wasm IR.

---
+++ src/ir.rs
@@ -0,0 +1,39 @@
+pub type Idx = u32;
+pub type TypeIdx = Idx;
+pub type LocalIdx = Idx;
+pub type FuncIdx = Idx;
+
+pub struct Module {
+    pub types: Vec<Type>,
+    pub functions: Vec<Function>,
+    pub exports: Vec<Export>,
+}
+
+pub enum Type {
+    Function(FuncType),
+}
+
+pub struct FuncType {
+    pub params: Vec<ValType>,
+    pub results: Vec<ValType>,
+}
+
+pub enum ValType {
+    I64,
+    F64,
+}
+
+pub struct Function {
+    ty: TypeIdx,
+    body: Vec<Instruction>,
+}
+
+pub enum Instruction {
+    I64Add,
+    LocalGet(LocalIdx),
+    End,
+}
+
+pub enum Export {
+    Func(FuncIdx),
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 pub mod ast;
 pub mod emit;
 pub mod error;
+pub mod ir;
 pub mod lexer;
 pub mod parser;
 pub mod pretty;
